33d854362cc08c0ed81188751c4d9ef4b0fd0a67
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2024-02-01 10:17:36 +0300

    added partnership registration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ButtonLink } from "./components/ui/button-link";
 // md:bg-[url('./assets/bg.png')] bg-[url('./assets/bg-small-one.png')]
 function App() {
     return (
-        <div className="h-screen w-full bg-slate-100 bg-cover md:bg-[url('./assets/bg.png')] bg-[url('./assets/bg-small-one.png')] md:bg-cover bg-center">
+        <div className="h-screen w-full bg-slate-100 bg-cover  bg-[url('./assets/bg-background.jpg')] md:bg-cover bg-center">
             <div className="w-full h-screen flex justify-center items-center ">
                 {/* <div className="flex justify-center items-center -mt-40 w-1/2">
                     
@@ -12,7 +12,7 @@ function App() {
                     <div className="absolute top-0 sm:top-0 m-auto left-0 right-0 md:flex md:justify-center md:items-center lg:w-1/2">
                         <img src={Parents} alt="parents" />
                     </div>
-                    <div className="flex flex-col justify-center items-center z-10  sm:mt-52 text-white">
+                    <div className="flex flex-col justify-center items-center z-10  sm:mt-52 ">
                         <h1 className="md:text-8xl text-5xl font-black  z-10">Nairobi,Kenya</h1>
                         <h2 className="md:text-4xl text-lg font-extrabold ">Healing and Deliverance Crusade</h2>
                         <h5>With</h5>
