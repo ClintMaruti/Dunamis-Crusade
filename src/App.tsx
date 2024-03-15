19fd83bc7cd7a1f92d8bb12db34032bc6e3a9420
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2024-03-15 15:36:09 +0300

    changed partners link to point to whatsapp group link

--- src/App.tsx
+++ src/App.tsx
@@ -30,35 +30,14 @@ function App() {
                 </div>
                 <div className="flex flex-col gap-2 bg-white p-3 rounded-md shadow-md">
                     <h5>Click to be a partner</h5>
-                    <ButtonLink to="https://forms.gle/4Lu7GSLEzF1oJAkB7" className="cursor-pointer">
+                    <ButtonLink to="https://chat.whatsapp.com/GhADCxo7bIt4cYOIUwAj3I" className="cursor-pointer">
                         Patnership Registration
                     </ButtonLink>
                 </div>
             </div>
-            <div className=" w-full bottom-3 flex flex-col justify-center items-center">
-                {/* <div className="w-full flex justify-center items-center py-2 px-2 bg-primary bg-yellow-300 my-10">
-                    <p className="text-center md:text-start">
-                        <span className="font-bold">Dates:</span> 16th - 17th May | <span className="font-bold">Venue:</span> Kasarani Stadium Nairobi
-                    </p>
-                </div> */}
-            </div>
+            <div className=" w-full bottom-3 flex flex-col justify-center items-center"></div>
         </div>
     );
 }
 
 export default App;
-
-// bg-secondaryImg bg-cover bg-center
-{
-    /* <div className="w-3/4 md:w-1/2">
-                    <img src={Parents} alt="parents" />
-                </div> */
-}
-{
-    /* <div className="flex flex-col justify-center items-center">
-                    <h1 className="md:text-8xl text-5xl font-black  z-10">Nairobi,Kenya</h1>
-                    <h2 className="md:text-4xl text-lg font-extrabold ">Healing and Deliverance Crusade</h2>
-                    <h5>With</h5>
-                    <h5 className="text-base sm:text-lg font-bold ">Dr. Pst. Paul and Dr. Mrs. Becky Enenche</h5>
-                </div> */
-}
